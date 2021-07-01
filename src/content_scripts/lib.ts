3aa6ca1910e25ddea4227abd8602ec22d71fa521
Jian Li <kdlijian@gmail.com>
2021-07-02 02:36:41 +0800

    better sentence close detection

--- src/content_scripts/lib.ts
+++ src/content_scripts/lib.ts
@@ -177,7 +177,7 @@ export function getSceneSentence(parent: Node, selectText: string): string {
 	let end = text.length
 	let found = false
 	const delimiter = /^[.!?。！？]$/
-	const close = /^[\s"')]$/
+	const close = /^[\s"')A-Z]$/
 	const chineseDelimeter = /^[。！？]$/
 	for (let i = 0; i < text.length; i++) {
 		if (delimiter.test(text[i]) && found == false && (close.test(text[i + 1]) || chineseDelimeter.test(text[i]))) {
