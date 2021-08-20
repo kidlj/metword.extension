1da79dc691b6df349d717624c80cb498175966de
Jian Li <kdlijian@gmail.com>
2021-08-21 00:04:47 +0800

    fix api endpoints

--- src/config.ts
+++ src/config.ts
@@ -7,21 +7,21 @@ export interface Config {
 const env = process.env.NODE_ENV
 
 const development = {
-	loginURL: "http://words.metaphor.com:8080/account/login",
-	meetsURL: "http://words.metaphor.com:8080/meet/times",
-	queryURL: "http://words.metaphor.com:8080/word?word=",
-	addSceneURL: "http://words.metaphor.com:8080/scene",
-	forgetSceneURL: "http://words.metaphor.com:8080/scene?id=",
-	knowURL: "http://words.metaphor.com:8080/meet/toggle?id="
+	loginURL: "http://words.metaphor.com:9000/api/account/login",
+	meetsURL: "http://words.metaphor.com:9000/api/meet/times",
+	queryURL: "http://words.metaphor.com:9000/api/word?word=",
+	addSceneURL: "http://words.metaphor.com:9000/api/scene",
+	forgetSceneURL: "http://words.metaphor.com:9000/api/scene?id=",
+	knowURL: "http://words.metaphor.com:9000/api/meet/toggle?id="
 }
 
 const production = {
-	loginURL: "https://www.metwords.com/account/login",
-	meetsURL: "https://www.metwords.com/meet/times",
-	queryURL: "https://www.metwords.com/word?word=",
-	addSceneURL: "https://www.metwords.com/scene",
-	forgetSceneURL: "https://www.metwords.com/scene?id=",
-	knowURL: "https://www.metwords.com/meet/toggle?id="
+	loginURL: "https://www.metwords.com/api/account/login",
+	meetsURL: "https://www.metwords.com/api/meet/times",
+	queryURL: "https://www.metwords.com/api/word?word=",
+	addSceneURL: "https://www.metwords.com/api/scene",
+	forgetSceneURL: "https://www.metwords.com/api/scene?id=",
+	knowURL: "https://www.metwords.com/api/meet/toggle?id="
 }
 
 const config: Config = {
