44d92e8715dada3b6fa21ff9456e480b02f95244
Jian Li <kdlijian@gmail.com>
2022-10-13 08:12:55 +0800

    fix url configurations

--- src/config.ts
+++ src/config.ts
@@ -7,49 +7,41 @@ export interface Config {
 const env = process.env.NODE_ENV
 
 const development = {
-	meetsURL: "http://localhost:8080/meet/times",
-	queryURL: "http://localhost:8080/word?word=",
-	addSceneURL: "http://localhost:8080/scene",
-	forgetSceneURL: "http://localhost:8080/scene?id=",
-	knowURL: "http://localhost:8080/meet/toggle?id=",
-	wordsURL: "http://localhost:8080/",
+	meetsURL: "http://localhost:8080/api/meet/times",
+	knowURL: "http://localhost:8080/api/meet/toggle?id=",
+	queryURL: "http://localhost:8080/api/word?word=",
+	addSceneURL: "http://localhost:8080/api/scene",
+	forgetSceneURL: "http://localhost:8080/api/scene?id=",
 
 	articleStateURL: "http://localhost:8080/api/collection/state",
 	collectionURL: "http://localhost:8080/api/collection",
-	collectionsURL: "http://localhost:8080/collection/",
 
 	feedStateURL: "http://localhost:8080/api/feed/state",
-	feedURL: "http://localhost:8080/feed?id=",
 	subscribeURL: "http://localhost:8080/api/feed",
 
-	shareURL: "http://localhost:8080/api/share",
-
-	homeURL: "http://localhost:8080/",
-
-	audioURL: "https://static.metword.co/audio"
+	wordsURL: "http://localhost:8080/",
+	collectionsURL: "http://localhost:8080/collection/",
+	feedURL: "http://localhost:8080/feed?id=",
+	audioURL: "https://media.metword.co/audio"
 }
 
 const production = {
-	meetsURL: "https://app.metword.co/meet/times",
-	queryURL: "https://app.metword.co/word?word=",
-	addSceneURL: "https://app.metword.co/scene",
-	forgetSceneURL: "https://app.metword.co/scene?id=",
-	knowURL: "https://app.metword.co/meet/toggle?id=",
-	wordsURL: "https://app.metword.co/",
+	meetsURL: "https://app.metword.co/api/meet/times",
+	knowURL: "https://app.metword.co/api/meet/toggle?id=",
+	queryURL: "https://app.metword.co/api/word?word=",
+	addSceneURL: "https://app.metword.co/api/scene",
+	forgetSceneURL: "https://app.metword.co/api/scene?id=",
 
 	articleStateURL: "https://app.metword.co/api/collection/state",
 	collectionURL: "https://app.metword.co/api/collection",
-	collectionsURL: "https://app.metword.co/collection/",
 
 	feedStateURL: "https://app.metword.co/api/feed/state",
-	feedURL: "https://app.metword.co/feed?id=",
 	subscribeURL: "https://app.metword.co/api/feed",
 
-	shareURL: "http://app.metword.co/api/share",
-
-	homeURL: "https://metword.co/",
-
-	audioURL: "https://static.metword.co/audio"
+	wordsURL: "https://app.metword.co/",
+	collectionsURL: "https://app.metword.co/collection/",
+	feedURL: "https://app.metword.co/feed?id=",
+	audioURL: "https://media.metword.co/audio"
 }
 
 const config: Config = {
